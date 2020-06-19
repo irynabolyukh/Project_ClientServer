d521002bb19aab18f994537bd90aadfad699ae35
irynabolyukh <iryna.boliukh@ukma.edu.ua>
2020-06-19 16:55:25 +0300

    tcp checked

--- src/main/java/client_server/client/StoreClientTCP.java
+++ src/main/java/client_server/client/StoreClientTCP.java
@@ -1,5 +1,4 @@
 package client_server.client;
-
 import com.google.common.primitives.UnsignedLong;
 import client_server.entities.MessageGenerator;
 import client_server.entities.Packet;
@@ -11,12 +10,14 @@ import java.net.InetAddress;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
 import java.util.concurrent.atomic.AtomicInteger;
-//CLIENT
+
 public class StoreClientTCP {
     private static final int CLIENT_PORT = 2222;
     private static final int RECONNECT_MAX = 3;
-    private static final int AMOUNT_OF_CLIENTS = 1000;
+    private static final int AMOUNT_OF_CLIENTS = 50;
 
+    //variables to check whether right amount of packets was (not) delivered
+    //AMOUNT_OF_CLIENTS = NUMBER_RECEIVED + NUMBER_DEAD
     private static final AtomicInteger NUMBER_RECEIVED = new AtomicInteger(0);
     private static final AtomicInteger NUMBER_DEAD = new AtomicInteger(0);
 
@@ -26,6 +27,7 @@ public class StoreClientTCP {
             final int srcID = i;//userId
             final int pktID = 1;
             final int reconnect_num = 1;
+
             new Thread(() -> {
                 try (final Socket socket = new Socket(InetAddress.getByName(null), CLIENT_PORT)) {
                     clientTCP(socket,srcID,pktID);
@@ -45,8 +47,8 @@ public class StoreClientTCP {
             clientTCP(socket, srcID, pktID);
         } catch (IOException e) {
             // e.printStackTrace();
-            System.out.println("Reconnecting");
-            System.out.println("SERVER IS OFFLINE!!!");
+            System.out.println("Reconnecting\tSERVER IS OFFLINE!!!");
+
             if(reconnect_num == RECONNECT_MAX){
                 NUMBER_DEAD.incrementAndGet();
                 System.out.println("SERVER IS DEAD:( \t\t NUMBER of DEAD connections: "+ NUMBER_DEAD);
@@ -54,6 +56,7 @@ public class StoreClientTCP {
             else{
                 int newPktId = pktID + 1;
                 int reconnect = reconnect_num + 1;
+
                 reconnect(srcID, newPktId, reconnect);
             }
         }
@@ -68,16 +71,16 @@ public class StoreClientTCP {
         outputStream.flush();
         Packet packetFromUser = new Packet(message_from_user);
 
-        final byte[] inputMessage = new byte[300];
+        final byte[] inputMessage = new byte[20000];
         final int messageSize = inputStream.read(inputMessage);
         byte[] fullPacket = new byte[messageSize];
         System.arraycopy(inputMessage, 0, fullPacket, 0, messageSize);
         Packet receivedPacket = new Packet(fullPacket);
 
         if(packetFromUser.getbPktId().equals(receivedPacket.getbPktId()))
-            System.out.println("CORRECT packet was sent!");
+            System.out.println("CORRECT response!");
         else
-            System.out.println("WRONG response");
+            System.out.println("WRONG response!");
 
         NUMBER_RECEIVED.incrementAndGet();
         System.out.println("Response from server: " + new String(receivedPacket.getBMsq().getMessage(), StandardCharsets.UTF_8)
